cb80fc1a3b15d39d82ef949bf295c7db1a6d717b
Villu Ruusmann <villu.ruusmann@gmail.com>
2014-04-08 21:26:49 +0100

    Added 'mean' extension function

---
+++ pmml-extension/src/test/java/org/jpmml/evaluator/functions/MeanFunctionTest.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2014 Villu Ruusmann
+ *
+ * This file is part of JPMML-Evaluator
+ *
+ * JPMML-Evaluator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Evaluator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Evaluator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.evaluator.functions;
+
+import java.util.*;
+
+import org.jpmml.evaluator.*;
+import org.jpmml.evaluator.FieldValue;
+
+import org.dmg.pmml.*;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+public class MeanFunctionTest {
+
+	@Test
+	public void evaluate(){
+		List<Double> values = Arrays.asList(1d, 2d, 3d, 4d, 5d);
+
+		assertEquals(3d, evaluate(values));
+	}
+
+	static
+	private Number evaluate(List<Double> values){
+		Function mean = new MeanFunction();
+
+		List<FieldValue> arguments = Arrays.asList(FieldValueUtil.create(DataType.DOUBLE, OpType.CONTINUOUS, values));
+
+		return (mean.evaluate(arguments)).asNumber();
+	}
+}
