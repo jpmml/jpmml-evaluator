6c67bfc3cf1252589a10f24964753193fcbd05c9
Villu Ruusmann <villu.ruusmann@gmail.com>
2014-10-12 21:44:08 +0300

    Testing calculated affinities

--- pmml-rattle/src/test/R/ClusteringTest.R
+++ pmml-rattle/src/test/R/ClusteringTest.R
@@ -6,9 +6,16 @@ irisData = readCsv("csv/Iris.csv")
 
 numericIrisData = irisData[, c("Sepal_Length", "Sepal_Width", "Petal_Length", "Petal_Width")]
 
-writeIris = function(clusters, file){
-	result = data.frame(clusters)
-	names(result) = c("predictedValue")
+writeIris = function(clusters, affinities, file){
+	result = NULL
+
+	if(is.null(affinities)){
+		result = data.frame("predictedValue" = clusters)
+	} else
+
+	{
+		result = data.frame("predictedValue" = clusters, affinities)
+	}
 
 	writeCsv(result, file)
 }
@@ -19,7 +26,7 @@ generateHierarchicalClusteringIris = function(){
 	saveXML(pmml(hcluster, centers = centers), "pmml/HierarchicalClusteringIris.pmml")
 
 	clusters = predict(hcluster, numericIrisData, numericIrisData, 10)
-	writeIris(clusters, "csv/HierarchicalClusteringIris.csv")
+	writeIris(clusters, NULL, "csv/HierarchicalClusteringIris.csv")
 }
 
 generateKMeansIris = function(){
@@ -27,9 +34,14 @@ generateKMeansIris = function(){
 
 	kmeans = kmeans(numericIrisData, 3)
 	saveXML(pmml(kmeans), "pmml/KMeansIris.pmml")
-	
+
+	affinity = function(center){
+		return (colSums(apply(numericIrisData, 1, function(x) { ((x - center) ^ 2) })))
+	}
+
 	clusters = predict(kmeans, numericIrisData)
-	writeIris(clusters, "csv/KMeansIris.csv")
+	affinities = data.frame("affinity_1" = affinity(kmeans$centers[1, ]), "affinity_2" = affinity(kmeans$centers[2, ]), "affinity_3" = affinity(kmeans$centers[3, ]))
+	writeIris(clusters, affinities, "csv/KMeansIris.csv")
 }
 
 generateHierarchicalClusteringIris()
