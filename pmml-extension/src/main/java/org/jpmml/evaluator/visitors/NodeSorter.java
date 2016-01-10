44ff17ab7ab21d4255e424252fc8a7019958abb2
Villu Ruusmann <villu.ruusmann@gmail.com>
2016-01-10 19:18:15 +0200

    Moved Visitor classes to 'pmml-extension' module

--- pmml-extension/src/main/java/org/jpmml/evaluator/visitors/NodeSorter.java
+++ pmml-extension/src/main/java/org/jpmml/evaluator/visitors/NodeSorter.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with JPMML-Evaluator.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.jpmml.rattle;
+package org.jpmml.evaluator.visitors;
 
 import java.util.Collections;
 import java.util.Comparator;
