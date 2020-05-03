f0ac6dd855b4d43b60d232a737fc414a955c47a4
Villu Ruusmann <villu.ruusmann@gmail.com>
2020-05-03 08:24:32 +0300

    Removed integration testing modules

--- pmml-rattle/src/test/R/common.R
+++
@@ -1,9 +0,0 @@
-setwd("../resources")
-
-readCsv = function(file){
-	return (read.csv(file = file, header = TRUE))
-}
-
-writeCsv = function(data, file){
-	write.table(data, file = file, sep = ",", quote = FALSE, row.names = FALSE, col.names = TRUE)
-}
