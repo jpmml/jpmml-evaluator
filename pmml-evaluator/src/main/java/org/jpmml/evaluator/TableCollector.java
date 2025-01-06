db148279c651b7c1852c3cfb2760ca888913d761
Villu Ruusmann <villu.ruusmann@gmail.com>
2025-01-06 12:31:54 +0200

    Added TableCollector class

---
+++ pmml-evaluator/src/main/java/org/jpmml/evaluator/TableCollector.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2025 Villu Ruusmann
+ *
+ * This file is part of JPMML-Evaluator
+ *
+ * JPMML-Evaluator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Evaluator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Evaluator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.evaluator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
+
+@IgnoreJRERequirement
+public class TableCollector implements Collector<Object, List<Object>, Table> {
+
+	public TableCollector(){
+	}
+
+	@Override
+	public Set<Collector.Characteristics> characteristics(){
+		return Collections.emptySet();
+	}
+
+	@Override
+	public Supplier<List<Object>> supplier(){
+		return () -> {
+			return new ArrayList<>();
+		};
+	}
+
+	@Override
+	public BiConsumer<List<Object>, Object> accumulator(){
+		return (elements, element) -> {
+
+			if(element instanceof Map<?, ?>){
+				Map<?, ?> map = (Map<?, ?>)element;
+			} else
+
+			if(element instanceof Exception){
+				Exception exception = (Exception)element;
+			} else
+
+			{
+				throw new IllegalArgumentException();
+			}
+
+			elements.add(element);
+		};
+	}
+
+	@Override
+	public BinaryOperator<List<Object>> combiner(){
+		return (left, right) -> {
+			left.addAll(right);
+
+			return left;
+		};
+	}
+
+	@Override
+	public Function<List<Object>, Table> finisher(){
+		return (elements) -> {
+			Table table = new Table(elements.size());
+
+			TableWriter tableWriter = createTableWriter(table);
+
+			for(Object element : elements){
+				tableWriter.next();
+
+				if(element instanceof Map<?, ?>){
+					Map<?, ?> map = (Map<?, ?>)element;
+
+					tableWriter.putAll((Map)map);
+				} else
+
+				if(element instanceof Exception){
+					Exception exception = (Exception)element;
+
+					tableWriter.put(exception);
+				} else
+
+				{
+					throw new IllegalArgumentException();
+				}
+			}
+
+			table.canonicalize();
+
+			return table;
+		};
+	}
+
+	protected TableWriter createTableWriter(Table table){
+		return new TableWriter(table);
+	}
+}
---
+++ pmml-evaluator/src/test/java/org/jpmml/evaluator/TableCollectorTest.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 2025 Villu Ruusmann
+ *
+ * This file is part of JPMML-Evaluator
+ *
+ * JPMML-Evaluator is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Evaluator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Evaluator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.evaluator;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+public class TableCollectorTest {
+
+	@Test
+	public void collectSequential(){
+		TableCollector tableCollector = new TableCollector(){
+
+			private int counter = 0;
+
+
+			@Override
+			public BinaryOperator<List<Object>> combiner(){
+				return (left, right) -> {
+					left.addAll(right);
+
+					this.counter += 1;
+
+					return left;
+				};
+			}
+
+			@Override
+			public Function<List<Object>, Table> finisher(){
+				assertTrue(this.counter == 0);
+
+				return super.finisher();
+			}
+		};
+
+		Table table = IntStream.range(0, 1000)
+			.sequential()
+			.mapToObj(createMapper())
+			.collect(tableCollector);
+
+		checkTable(table, 0, 1000);
+	}
+
+	@Test
+	public void collectParallel(){
+		TableCollector tableCollector = new TableCollector(){
+
+			private int counter = 0;
+
+
+			@Override
+			public BinaryOperator<List<Object>> combiner(){
+				return (left, right) -> {
+					left.addAll(right);
+
+					this.counter += 1;
+
+					return left;
+				};
+			}
+
+			@Override
+			public Function<List<Object>, Table> finisher(){
+				assertTrue(this.counter > 0);
+
+				return super.finisher();
+			}
+		};
+
+		Table table = IntStream.range(0, 10 * 1000)
+			.parallel()
+			.mapToObj(createMapper())
+			.collect(tableCollector);
+
+		checkTable(table, 0, 10 * 1000);
+	}
+
+	static
+	private void checkTable(Table table, int begin, int end){
+		assertEquals(Arrays.asList("A", "B", "C"), table.getColumns());
+
+		assertEquals((end - begin), table.getNumberOfRows());
+		assertEquals(3, table.getNumberOfColumns());
+
+		assertTrue(table.hasExceptions());
+
+		TableReader tableReader = new TableReader(table);
+
+		for(int i = begin; i < end; i++){
+			tableReader.next();
+
+			int value = i;
+
+			if(value % 100 == 0){
+				assertNotNull(table.getException(i));
+
+				assertNull(tableReader.get("A"));
+				assertNull(tableReader.get("B"));
+				assertNull(tableReader.get("C"));
+			} else
+
+			{
+				assertNull(table.getException(i));
+
+				assertEquals((int)value, tableReader.get("A"));
+				assertEquals((double)value, tableReader.get("B"));
+				assertEquals(String.valueOf(value), tableReader.get("C"));
+			}
+		}
+	}
+
+	static
+	private IntFunction<Object> createMapper(){
+		return (value) -> {
+
+			if(value % 100 == 0){
+				return new IllegalArgumentException();
+			} else
+
+			{
+				Map<String, Object> result = new LinkedHashMap<>();
+
+				result.put("A", (int)value);
+				result.put("B", (double)value);
+				result.put("C", String.valueOf(value));
+
+				return result;
+			}
+		};
+	}
+}
